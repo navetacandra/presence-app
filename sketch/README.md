34f7cb481673c51540b248da93023302c87a68a3
navetacandra <naveta.cand@gmail.com>
2023-06-24 07:42:16 +0700

    fix query

--- sketch/README.md
+++ sketch/README.md
@@ -1,10 +1,24 @@
+## PIN
+
 | ESP     | OUT         |
 |---------|-------------|
 | D1      | LCD I2C SCL |
 | D2      | LCD I2C SDA |
 | D3      | RFID RST    |
-| D4      | RFID SDA    |
+| D8      | RFID SDA    |
 | D5      | RFID SCK    |
 | D6      | RFID MISO   |
 | D7      | RFID MOSI   |
-| D8      | BUZZER      |
+| D4      | BUZZER      |
+
+## Library Used
+
+| Library            | Repository                                                                              |
+| ------------------ | --------------------------------------------------------------------------------------- |
+| ESP8266 Web Server | [Repository](https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WebServer) |
+| ESP8266 WiFi       | [Repository](https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi)      |
+| Firebase ESP8266   | [Repository](https://github.com/mobizt/Firebase-ESP8266)                                |
+| LiquidCrystal_I2C  | [Repository](https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library)         |
+| NTPClient          | [Repository](https://github.com/arduino-libraries/NTPClient)                            |
+| SPI                | [Repository](https://github.com/esp8266/Arduino/tree/master/libraries/SPI)              |
+| MFRC522            | [Repository](https://github.com/Jorgen-VikingGod/ESP8266-MFRC522)                       |
--- sketch/firebase.ino
+++ sketch/firebase.ino
@@ -22,28 +22,22 @@ int addTag(String tag) {
   ref += tag;
   ref += "/card";
 
-  if (Firebase.getJSON(fbdo, "/pegawai", query)) {
-    if (fbdo.jsonString().length() <= 2) {
-      if (Firebase.getString(fbdo, ref)) {
-        if ((fbdo.to<String>()).length() < 1) {
-          if (Firebase.setString(fbdo, ref, tag)) {
-            return 200;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
-        } else {
-          return 406;
-        }
+  Firebase.getJSON(fbdo, "/pegawai", query);
+
+  if (fbdo.jsonString().length() <= 2) {
+    Firebase.getString(fbdo, ref);
+    if ((fbdo.to<String>()).length() < 1) {
+      if (Firebase.setString(fbdo, ref, tag)) {
+        return 200;
       } else {
         Serial.println(fbdo.errorReason());
         return 500;
       }
+    } else {
+      return 406;
     }
-    return 304;
   } else {
-    Serial.println(fbdo.errorReason());
-    return 500;
+    return 304;
   }
 }
 
@@ -77,103 +71,76 @@ int presentTag(String tag, bool active, int startPresent[2], int endPresent[2], 
   // Get isActive
   if (active) {
     // Check is card tag registered
-    if (Firebase.getJSON(fbdo, "/pegawai", query)) {
-      if (fbdo.jsonString().length() <= 2) {
-        return 404;
-      }
-      String id;
-      FirebaseJson &tempPegawai = fbdo.jsonObject();
-      FirebaseJsonData tempData;
-      FirebaseJson data;
+    Firebase.getJSON(fbdo, "/pegawai", query);
 
-      size_t len = tempPegawai.iteratorBegin();
-      String key, value = "";
-      int type = 0;
-      tempPegawai.iteratorGet(0, type, key, value);
-      if (value.startsWith("{\"")) {
-        data.set("id", key);
-        id = key;
-      }
-      tempPegawai.iteratorEnd();
+    if (fbdo.jsonString().length() <= 2) {
+      return 404;
+    }
+    String id;
+    FirebaseJson &tempPegawai = fbdo.jsonObject();
+    FirebaseJsonData tempData;
+    FirebaseJson data;
 
-      if (Firebase.getJSON(fbdo, pegawaiRef + id)) {
-        FirebaseJson &fdata = fbdo.to<FirebaseJson>();
-        data = fdata;
-      } else {
-        Serial.println(fbdo.errorReason());
-        if (fbdo.errorReason() != "path not exist") {
+    size_t len = tempPegawai.iteratorBegin();
+    String key, value = "";
+    int type = 0;
+    tempPegawai.iteratorGet(0, type, key, value);
+    if (value.startsWith("{\"")) {
+      data.set("id", key);
+      id = key;
+    }
+    tempPegawai.iteratorEnd();
+
+    if (Firebase.getJSON(fbdo, pegawaiRef + id)) {
+      FirebaseJson &fdata = fbdo.to<FirebaseJson>();
+      data = fdata;
+    } else {
+      Serial.println(fbdo.errorReason());
+      if (fbdo.errorReason() != "path not exist") {
+        return 500;
+      }
+    }
+
+    // current time to string
+    String time = (String(currentHour).length() < 2 ? "0" + String(currentHour) : String(currentHour)) + ":" + (String(currentMinute).length() < 2 ? "0" + String(currentMinute) : String(currentMinute));
+
+    // Equal/above startPresent hour and equal/below endPresent hour
+    if (currentHour >= startPresent[0] && currentHour <= endPresent[0]) {
+      data.get(tempData, "masuk");
+      // If hour equal startPresent hour and minutes below startPresent minutes
+      if (currentHour == startPresent[0] && currentMinute < startPresent[1]) {
+        return 0;
+      }
+      // If already presence
+      else if (tempData.to<String>().length() > 3) {
+        return 3041;
+      }
+      // If hour equal startPresent hour and minutes equal/above startPresent minutes
+      else if (currentHour == startPresent[0] && currentMinute >= startPresent[1]) {
+        data.set("status", "tepat");
+        data.set("masuk", time);
+        if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
+          // if (true) {
+          return 1;
+        } else {
+          Serial.println(fbdo.errorReason());
           return 500;
         }
       }
-
-      // current time to string
-      String time = (String(currentHour).length() < 2 ? "0" + String(currentHour) : String(currentHour)) + ":" + (String(currentMinute).length() < 2 ? "0" + String(currentMinute) : String(currentMinute));
-
-      // Equal/above startPresent hour and equal/below endPresent hour
-      if (currentHour >= startPresent[0] && currentHour <= endPresent[0]) {
-        data.get(tempData, "masuk");
-        // If hour equal startPresent hour and minutes below startPresent minutes
-        if (currentHour == startPresent[0] && currentMinute < startPresent[1]) {
-          return 0;
-        }
-        // If already presence
-        else if (tempData.to<String>().length() > 3) {
-          return 3041;
-        }
-        // If hour equal startPresent hour and minutes equal/above startPresent minutes
-        else if (currentHour == startPresent[0] && currentMinute >= startPresent[1]) {
-          data.set("status", "tepat");
-          data.set("masuk", time);
-          if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
-            // if (true) {
-            return 1;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
-        }
-        // If hour equal endPresent hour and minutes equal/below endPresent minutes
-        else if (currentHour == endPresent[0] && currentMinute <= endPresent[1]) {
-          data.set("status", "tepat");
-          data.set("masuk", time);
-          if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
-            // if (true) {
-            return 1;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
-        }
-        // If hour equal endPresent hour and minutes above endPresent minutes
-        else if (currentHour == endPresent[0] && currentMinute > endPresent[1]) {
-          data.set("status", "telat");
-          data.set("masuk", time);
-          if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
-            // if (true) {
-            return 3;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
+      // If hour equal endPresent hour and minutes equal/below endPresent minutes
+      else if (currentHour == endPresent[0] && currentMinute <= endPresent[1]) {
+        data.set("status", "tepat");
+        data.set("masuk", time);
+        if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
+          // if (true) {
+          return 1;
         } else {
-          data.set("status", "tepat");
-          data.set("masuk", time);
-          if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
-            // if (true) {
-            return 1;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
+          Serial.println(fbdo.errorReason());
+          return 500;
         }
       }
-
-      // Between endPresent and startHome
-      if (currentHour > endPresent[0] && currentHour < startHome[0]) {
-        data.get(tempData, "masuk");
-        if (tempData.to<String>().length() > 3) {
-          return 3041;
-        }
+      // If hour equal endPresent hour and minutes above endPresent minutes
+      else if (currentHour == endPresent[0] && currentMinute > endPresent[1]) {
         data.set("status", "telat");
         data.set("masuk", time);
         if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
@@ -183,30 +150,54 @@ int presentTag(String tag, bool active, int startPresent[2], int endPresent[2], 
           Serial.println(fbdo.errorReason());
           return 500;
         }
-      }
-
-      // Equal/above startHome hour and equal/below endHome hour
-      if (currentHour >= startHome[0] && currentHour <= endHome[0]) {
-        data.get(tempData, "pulang");
-        // If hour equal startHome hour and minutes below startHome minutes OR hour equal endHome hour and minutes above endHome minutes
-        if ((currentHour == startHome[0] && currentMinute < startHome[1]) || (currentHour == endHome[0] && currentMinute > endHome[1])) {
-          return 0;
-        } else if (tempData.to<String>().length() > 3) {
-          return 3042;
+      } else {
+        data.set("status", "tepat");
+        data.set("masuk", time);
+        if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
+          // if (true) {
+          return 1;
         } else {
-          data.set("pulang", time);
-          if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
-            // if (true) {
-            return 2;
-          } else {
-            Serial.println(fbdo.errorReason());
-            return 500;
-          }
+          Serial.println(fbdo.errorReason());
+          return 500;
         }
       }
-    } else {
-      Serial.println(fbdo.errorReason());
-      return 500;
+    }
+
+    // Between endPresent and startHome
+    if (currentHour > endPresent[0] && currentHour < startHome[0]) {
+      data.get(tempData, "masuk");
+      if (tempData.to<String>().length() > 3) {
+        return 3041;
+      }
+      data.set("status", "telat");
+      data.set("masuk", time);
+      if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
+        // if (true) {
+        return 3;
+      } else {
+        Serial.println(fbdo.errorReason());
+        return 500;
+      }
+    }
+
+    // Equal/above startHome hour and equal/below endHome hour
+    if (currentHour >= startHome[0] && currentHour <= endHome[0]) {
+      data.get(tempData, "pulang");
+      // If hour equal startHome hour and minutes below startHome minutes OR hour equal endHome hour and minutes above endHome minutes
+      if ((currentHour == startHome[0] && currentMinute < startHome[1]) || (currentHour == endHome[0] && currentMinute > endHome[1])) {
+        return 0;
+      } else if (tempData.to<String>().length() > 3) {
+        return 3042;
+      } else {
+        data.set("pulang", time);
+        if (Firebase.setJSON(fbdo, pegawaiRef + id, data)) {
+          // if (true) {
+          return 2;
+        } else {
+          Serial.println(fbdo.errorReason());
+          return 500;
+        }
+      }
     }
   } else {
     return 400;
